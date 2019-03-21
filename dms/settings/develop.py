1bdf7e1b593ce4d600c4c22ebd761c947de58e09
qbzhhh <qbz1993@hotmail.com>
2019-03-21 09:12:45 +0800

    11

---
+++ dms/settings/develop.py
@@ -0,0 +1,13 @@
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+from .base import * #NOQA
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
